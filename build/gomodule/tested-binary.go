756f529f0e0f728965a2c26690f4bc66579abb7a
vano7577 <vano7577@gmail.com>
2021-03-07 15:11:40 +0200

    tested-binary

--- build/gomodule/tested-binary.go
+++ build/gomodule/tested-binary.go
@@ -1,15 +1,80 @@
 package gomodule
 
-import "github.com/google/blueprint"
-
+import (
+	"fmt"
+	"github.com/google/blueprint"
+	"github.com/roman-mazur/bood"
+	"path"
+)
+var (
+	pctx = blueprint.NewPackageContext("github.com/roman-mazur/bood/gomodule")
+	goBuild = pctx.StaticRule("binaryBuild", blueprint.RuleParams{
+		Command:     "cd $workDir && go build -o $outputPath $pkg",
+		Description: "build go command $pkg",
+	}, "workDir", "outputPath", "pkg")
+	goVendor = pctx.StaticRule("vendor", blueprint.RuleParams{
+		Command:     "cd $workDir && go mod vendor",
+		Description: "vendor dependencies of $name",
+	}, "workDir", "name")
+	)
 type testedBinaryModule struct {
 	blueprint.SimpleName
-
 	properties struct {
-		// TODO: Визначте поля структури, щоб отримати дані з визначень у файлі build.bood
+		Pkg string
+		Srcs []string
+		SrcsExclude []string
+		VendorFirst bool
+		Deps []string
 	}
 }
 
 func (tb *testedBinaryModule) GenerateBuildActions(ctx blueprint.ModuleContext) {
-	// TODO: Імплементууйте генерацію правил збірки для ninja-файла.
+	name := ctx.ModuleName()
+	config := bood.ExtractConfig(ctx)
+	config.Debug.Printf("Adding build actions for go binary module '%s'", name)
+
+	outputPath := path.Join(config.BaseOutputDir, "bin", name)
+
+	var inputs []string
+	inputErors := false
+	for _, src := range tb.properties.Srcs {
+		if matches, err := ctx.GlobWithDeps(src, tb.properties.SrcsExclude); err == nil {
+			inputs = append(inputs, matches...)
+		} else {
+			ctx.PropertyErrorf("srcs", "Cannot resolve files that match pattern %s", src)
+			inputErors = true
+		}
+	}
+	if inputErors {
+		return
+	}
+
+	if tb.properties.VendorFirst {
+		vendorDirPath := path.Join(ctx.ModuleDir(), "vendor")
+		ctx.Build(pctx, blueprint.BuildParams{
+			Description: fmt.Sprintf("Vendor dependencies of %s", name),
+			Rule:        goVendor,
+			Outputs:     []string{vendorDirPath},
+			Implicits:   []string{path.Join(ctx.ModuleDir(), "go.mod")},
+			Optional:    true,
+			Args: map[string]string{
+				"workDir": ctx.ModuleDir(),
+				"name":    name,
+			},
+		})
+		inputs = append(inputs, vendorDirPath)
+	}
+
+	ctx.Build(pctx, blueprint.BuildParams{
+		Description: fmt.Sprintf("Build %s as Go binary", name),
+		Rule:        goBuild,
+		Outputs:     []string{outputPath},
+		Implicits:   inputs,
+		Args: map[string]string{
+			"outputPath": outputPath,
+			"workDir":    ctx.ModuleDir(),
+			"pkg":        tb.properties.Pkg,
+		},
+	})
+
 }
